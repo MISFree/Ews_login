c8e48bf9efbcbfea38ace02f057720b7837d35d4
MISFree <90303323+MISFree@users.noreply.github.com>
2026-03-17 15:13:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = 3004;
 
 /* ================= LOGO PATH ================= */
 
-const logoPath = path.join(__dirname, "public/Mobile_CMD/newlogo.gif");
+const logoPath = path.join(__dirname, "/newlogo.gif");
 
 
 /* ================= SMTP CONFIG ================= */
